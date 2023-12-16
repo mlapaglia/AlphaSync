158f8071073172736510d88b77512c02acb16c67
Matt LaPaglia <mlapaglia@gmail.com>
2023-12-16 08:03:44 -0500

    more stability

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.alphasync"
         minSdk = 28
         targetSdk = 34
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 3
+        versionName = "1.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
--- app/src/main/java/com.alphasync/settings/CameraSettings.kt
+++
@@ -1,5 +0,0 @@
-package com.alphasync.settings
-
-import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.combine
-
